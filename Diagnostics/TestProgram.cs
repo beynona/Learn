629630f9d128c9e95e7045ff36f09e9e19519243
Evgeniy <mr.optimuc1997@gmail.com>
2021-04-18 13:10:54 +0300

    Diagnostics

---
+++ Diagnostics/TestProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Diagnostics
+{
+    class TestProgram
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        //Время выполнения куска кода (Пространство имён System.Diagnostics;)
+        void TimeCode()
+        {
+            var watch = new Stopwatch();
+            watch = new Stopwatch();
+            watch.Start();
+            //код
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds); // 1 секунда = 1000 Миллисекунд
+        }
+    }
+}
